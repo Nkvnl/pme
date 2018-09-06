32d39ca0c12a3d3b9a03180b63a663c19062cdeb
Niek van Losenoord <niek_losenoord@hotmail.com>
2018-09-06 17:54:59 +0000

    sg

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.post("/send", (req, res) => {
         port: 587,
         auth: {
             user: 'mailserver163@gmail.com',
-            pass: 'ma1ls3rv3r'
+            pass: 'HIB56JIB79BONc'
         }
     });
     // setup email data with unicode symbols
